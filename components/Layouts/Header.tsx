a3fca970a89ac2fbf68aebabeea0c3337d5e7c1d
Warsono <warsono16694@gmail.com>
2022-06-08 14:35:45 +0700

    chore: add header

---
+++ components/Layouts/Header.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <div className="py-3 border-b bg-white">
+      <div className="flex gap-4 container mx-auto items-center justify-between">
+        <Image
+          src="https://gits.id/wp-content/uploads/2020/09/Logo-Main.png"
+          alt="Logo GITS"
+          className="h-10"
+          width={80}
+          height={35}
+        />
+
+        <nav className="flex gap-4 items-center">
+          <Link className="hover:font-semibold" href="/">
+            <a>Home</a>
+          </Link>
+          <Link className="hover:font-semibold" href="/about">
+            <a>About</a>
+          </Link>
+          <Link className="hover:font-semibold" href="/components">
+            <a>Components</a>
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
+}
