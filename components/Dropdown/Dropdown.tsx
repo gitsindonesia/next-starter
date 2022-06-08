fba92ee959d84b33ebeb0f7b532f7e05cd07d67c
Warsono <warsono16694@gmail.com>
2022-06-08 17:09:21 +0700

    chore(dropdown): working dropdown

--- components/Dropdown/Dropdown.tsx
+++ components/Dropdown/Dropdown.tsx
@@ -1,29 +1,24 @@
 import { Menu, Transition } from '@headlessui/react';
 import { Fragment, useEffect, useRef, useState } from 'react';
-import { Icon } from '@iconify/react';
+import Button from '../Button/Button';
 
 export type DropdownProps = {
   label?: string;
   right?: boolean;
+  children?: React.ReactNode;
+  button?: React.ReactNode;
 };
 
-export default function Dropdown({
-  label = 'Options',
+export function Dropdown({
   right = false,
+  label,
+  children,
+  button,
 }: DropdownProps) {
   return (
     <div className="">
       <Menu as="div" className="relative inline-block text-left">
-        <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-80 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-            {label}
-            <Icon
-              icon="mdi-light:home"
-              className="ml-2 -mr-1 h-5 w-5 text-violet-200 hover:text-violet-100"
-              aria-hidden="true"
-            />
-          </Menu.Button>
-        </div>
+        <div>{button ?? <Menu.Button as={Button}>{label}</Menu.Button>}</div>
         <Transition
           as={Fragment}
           enter="transition ease-out duration-100"
@@ -38,135 +33,33 @@ export default function Dropdown({
               right ? 'right-0' : 'left-0'
             } mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`}
           >
-            <div className="px-1 py-1 ">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <Icon
-                        icon="mdi-light:home"
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <Icon
-                        icon="mdi-light:home"
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Edit
-                  </button>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <Icon
-                        icon="mdi-light:home"
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <Icon
-                        icon="mdi-light:home"
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Duplicate
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-            <div className="px-1 py-1">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <Icon
-                        icon="mdi-light:home"
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <Icon
-                        icon="mdi-light:home"
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Archive
-                  </button>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <Icon
-                        icon="mdi-light:home"
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <Icon
-                        icon="mdi-light:home"
-                        className="mr-2 h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Move
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-            <div className="px-1 py-1">
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    className={`${
-                      active ? 'bg-violet-500 text-white' : 'text-gray-900'
-                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                  >
-                    {active ? (
-                      <Icon
-                        icon="mdi-light:home"
-                        className="mr-2 h-5 w-5 text-violet-400"
-                        aria-hidden="true"
-                      />
-                    ) : (
-                      <Icon
-                        icon="mdi-light:home"
-                        className="mr-2 h-5 w-5 text-violet-400"
-                        aria-hidden="true"
-                      />
-                    )}
-                    Delete
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
+            {children}
           </Menu.Items>
         </Transition>
       </Menu>
     </div>
+  );
+}
+
+export type DropdownItemProps = {
+  icon?: React.ReactNode;
+  text?: string;
+  children?: React.ReactNode | string;
+};
+
+export function DropdownItem({ icon, children, text }: DropdownItemProps) {
+  return (
+    <Menu.Item>
+      {({ active }) => (
+        <button
+          className={`${
+            active ? 'bg-blue-600 text-white' : 'text-gray-900'
+          } group flex w-full items-center rounded-md px-2 py-2`}
+        >
+          {icon}
+          {children ?? text}
+        </button>
+      )}
+    </Menu.Item>
   );
 }
