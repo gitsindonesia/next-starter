46898dd7e696de1df369f9b943f3dad84d67e146
Warsono <warsono16694@gmail.com>
2022-06-08 14:48:10 +0700

    chore: footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '../components/Layouts/Header';
+import Footer from '../components/Layouts/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
