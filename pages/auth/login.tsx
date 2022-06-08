793a8ebe0c43f574865c7a87d4f0c07e956e4080
Warsono <warsono16694@gmail.com>
2022-06-08 18:22:26 +0700

    chore: login page

---
+++ pages/auth/login.tsx
@@ -0,0 +1,53 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Button from '../../components/Button/Button';
+
+export default function Login() {
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+      </Head>
+      <div className="container mx-auto py-6 sm:h-screen flex items-center justify-center">
+        <form className="w-3/12 mx-auto rounded-lg border p-6">
+          <div>
+            <h1 className="text-3xl font-semibold">Login</h1>
+            <p className="text-gray-500 mt-1">Please enter your credentials</p>
+          </div>
+
+          <div className="mt-4">
+            <label className="block mb-1 font-medium" htmlFor="username">
+              Username
+            </label>
+            <input
+              id="username"
+              type="text"
+              placeholder="Username"
+              className="w-full px-3 py-2 rounded-md transition duration-300"
+            />
+          </div>
+          <div className="mt-4">
+            <label className="block mb-1 font-medium" htmlFor="password">
+              Password
+            </label>
+            <input
+              id="password"
+              type="text"
+              placeholder="Password"
+              className="w-full px-3 py-2 rounded-md transition duration-300"
+            />
+          </div>
+
+          <div className="mt-4 flex gap-4 justify-between items-center">
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+            <Link href="/auth/password/forgot">
+              <a className="font-medium text-blue-600">Forgot Password</a>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
