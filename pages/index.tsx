892f5758c75cc7c0ee5cafa2a7fb7fc31393bc2d
Warsono <warsono16694@gmail.com>
2022-06-08 17:47:56 +0700

    chore: doc title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto py-4 sm:py-32 grid place-items-center">
-      <div className="text-center">
-        <h1 className="text-4xl font-semibold">
-          Welcome to your <span className="text-blue-600">Next</span> Project!
-        </h1>
+    <>
+      <Head>
+        <title>GITS Next Starter</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="container mx-auto py-4 sm:py-32 grid place-items-center">
+        <div className="text-center">
+          <h1 className="text-4xl font-semibold">
+            Welcome to your <span className="text-blue-600">Next</span> Project!
+          </h1>
 
-        <p className="mt-5">You are ready to rock!</p>
+          <p className="mt-5">You are ready to rock!</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
