1330eceeb3a4b8a32da9999d7037dcc61d5a02c2
Warsono <warsono16694@gmail.com>
2022-06-08 14:48:18 +0700

    chore: code smell

--- components/Layouts/Footer.tsx
+++ components/Layouts/Footer.tsx
@@ -1,6 +1,3 @@
-import Image from 'next/image';
-import Link from 'next/link';
-
 export default function Footer() {
   return (
     <footer className="flex gap-4 container mx-auto items-center justify-center  py-2 text-sm">
