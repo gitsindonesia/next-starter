068234d6949a990a3d22ace61004b5f720674a65
Warsono <warsono16694@gmail.com>
2022-06-08 17:33:12 +0700

    chore(Button): outlined, text, disabled

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -2,22 +2,39 @@ import React from 'react';
 
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Button, { ButtonSizes, ButtonVariants, sizes, variants } from './Button';
+import Button, {
+  ButtonSizes,
+  ButtonVariants,
+  getVariants,
+  sizes,
+} from './Button';
 
 export default {
   title: 'Components/Button',
   component: Button,
 } as ComponentMeta<typeof Button>;
 
-export const Variants: ComponentStory<typeof Button> = () => (
+const Template: ComponentStory<typeof Button> = (args) => (
   <div className="flex items-end gap-2">
-    {Object.keys(variants).map((variant) => (
-      <Button variant={variant as ButtonVariants} key={variant}>
+    {getVariants().map((variant) => (
+      <Button variant={variant as ButtonVariants} key={variant} {...args}>
         {variant}
       </Button>
     ))}
   </div>
 );
+
+export const Variants = Template.bind({});
+Variants.args = {};
+
+export const Outlined = Template.bind({});
+Outlined.args = { outlined: true };
+
+export const Text = Template.bind({});
+Text.args = { text: true };
+
+export const Disabled = Template.bind({});
+Disabled.args = { disabled: true };
 
 export const Sizes: ComponentStory<typeof Button> = () => (
   <div className="flex items-end gap-2">
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -14,24 +14,54 @@ export type ButtonProps = {
   children: React.ReactNode;
   variant?: ButtonVariants;
   size?: ButtonSizes;
+  outlined?: boolean;
+  text?: boolean;
+  disabled?: boolean;
 };
 
-export const variants = {
-  default:
-    'bg-white text-slate-800 border-slate-400 hover:border-slate-600 hover:text-slate-800',
-  primary:
-    'text-white bg-blue-600 border-blue-600 hover:bg-blue-700 hover:border-blue-700',
-  secondary:
-    'text-white bg-gray-800 border-gray-800 hover:bg-gray-900 hover:border-gray-900',
-  info: 'text-white bg-sky-600 border-sky-600 hover:bg-sky-700 hover:border-sky-700',
-  warning:
-    'text-white bg-orange-600 border-orange-600 hover:bg-orange-700 hover:border-orange-700',
-  error:
-    'text-white bg-red-600 border-red-600 hover:bg-red-700 hover:border-red-700',
+export const variants: Record<string, Record<ButtonVariants, string>> = {
+  default: {
+    default:
+      'bg-white text-slate-800 border-slate-400 hover:border-slate-600 hover:text-slate-800',
+    primary:
+      'text-white bg-blue-600 border-blue-600 hover:bg-blue-700 hover:border-blue-700 hover:text-white',
+    secondary:
+      'text-white bg-gray-800 border-gray-800 hover:bg-gray-900 hover:border-gray-900 hover:text-white',
+    info: 'text-white bg-sky-600 border-sky-600 hover:bg-sky-700 hover:border-sky-700 hover:text-white',
+    warning:
+      'text-white bg-orange-600 border-orange-600 hover:bg-orange-700 hover:border-orange-700 hover:text-white',
+    error:
+      'text-white bg-red-600 border-red-600 hover:bg-red-700 hover:border-red-700 hover:text-white',
+  },
+  outlined: {
+    default:
+      'bg-transparent text-slate-800 border-slate-400 hover:border-slate-600 hover:bg-slate-800 hover:text-white',
+    primary:
+      'bg-transparent text-blue-600 border-blue-600 hover:bg-blue-700 hover:border-blue-700 hover:text-white',
+    secondary:
+      'bg-transparent text-gray-800 border-gray-800 hover:bg-gray-900 hover:border-gray-900 hover:text-white',
+    info: 'bg-transparent text-sky-600 border-sky-600 hover:bg-sky-700 hover:border-sky-700 hover:text-white',
+    warning:
+      'bg-transparent text-orange-600 border-orange-600 hover:bg-orange-700 hover:border-orange-700 hover:text-white',
+    error:
+      'bg-transparent text-red-600 border-red-600 hover:bg-red-700 hover:border-red-700 hover:text-white',
+  },
+  text: {
+    default: 'text-slate-800 hover:text-slate-800',
+    primary: 'text-blue-600 hover:text-blue-700',
+    secondary: 'text-gray-600 hover:text-gray-700',
+    info: 'text-sky-600 hover:text-sky-700',
+    warning: 'text-orange-600 hover:text-orange-700',
+    error: 'text-red-600 hover:text-red-700',
+  },
 };
 
-function getVariantClass(variant: ButtonVariants) {
-  return variants[variant];
+export function getVariants() {
+  return Object.keys(variants.default);
+}
+
+function getVariantClass(variant: ButtonVariants, style = 'default') {
+  return variants[style][variant];
 }
 
 export const sizes = {
@@ -46,21 +76,38 @@ function getSizeClass(size: ButtonSizes) {
   return sizes[size];
 }
 
+function getStyle(props: Partial<ButtonProps>) {
+  let style = 'default';
+  if (props.outlined) {
+    style = 'outlined';
+  } else if (props.text) {
+    style = 'text';
+  }
+  return style;
+}
+
+const disabledClass =
+  'disabled:text-gray-400 disabled:border-gray-200 disabled:bg-gray-200 disabled:cursor-not-allowed';
+
 const Button = ({
   children,
   variant = 'default',
   size = 'md',
   ...props
 }: ButtonProps) => {
-  const variantClass = getVariantClass(variant);
+  const style = getStyle(props);
+  const variantClass = getVariantClass(variant, style);
   const sizeClass = getSizeClass(size);
 
-  const defaultClass =
-    'border transition duration-300 font-medium rounded-md inline-flex justify-center items-center';
+  const defaultClass = `${
+    !props.text ? 'border' : ''
+  } transition duration-300 font-medium rounded-md inline-flex justify-center items-center`;
 
   return (
     <button
-      className={[defaultClass, variantClass, sizeClass].join(' ')}
+      className={[defaultClass, variantClass, sizeClass, disabledClass].join(
+        ' '
+      )}
       {...props}
     >
       {children}
